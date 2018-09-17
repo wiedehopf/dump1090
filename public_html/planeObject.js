c91ae1b74f7124ed96b79630184ea99cc34cfc73
Matthias Wirth <matthias.wirth@gmail.com>
2018-09-17 17:28:55 +0200

    Webview: Ground state / altitude change
    Only draw the new state after the new point, don't assume the new state
    for the line to the previous point.

--- public_html/planeObject.js
+++ public_html/planeObject.js
@@ -232,18 +232,18 @@ PlaneObject.prototype.updateTrack = function(receiver_timestamp, last_timestamp)
         if ( (lastseg.ground && this.altitude !== "ground") ||
              (!lastseg.ground && this.altitude === "ground") || Math.abs(this.altitude - lastseg.altitude) >= 250 ) {
                 //console.log(this.icao + " ground state changed");
-                // Create a new segment as the ground state changed.
-                // assume the state changed halfway between the two points
-                // FIXME needs reimplementing post-google
+                // Create a new segment as the ground state or altitude changed.
+                // New state is only drawn for the track after the state has changed
+                // and we get a new position.
 
-                lastseg.fixed.appendCoordinate(projPrev);
-                this.track_linesegs.push({ fixed: new ol.geom.LineString([projPrev, projHere]),
+                lastseg.fixed.appendCoordinate(projHere);
+                this.track_linesegs.push({ fixed: new ol.geom.LineString([projHere]),
                                            feature: null,
                                            estimated: false,
                                            altitude: this.altitude,
                                            ground: (this.altitude === "ground") });
                 this.append_time = this.last_position_time;
-                this.history_size += 3;
+                this.history_size += 2;
                 return true;
         }
         
